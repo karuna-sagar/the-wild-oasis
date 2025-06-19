35a5e90f20a66da72e2a48f933fe56207c8c5f75
karuna_sagar <karunasagar129130@gmail.com>
2025-06-19 13:43:59 +0530

    refactor: Remove unused SocialLinks import from Sidebar component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
 import Uploader from "../data/Uploader";
-import SocialLinks from "./SocialLinks";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
